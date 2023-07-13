97ace0f4a79277dd2c85a6da4a71521a5e50f132
zhaoyuanda <1426593075@qq.com>
2023-07-13 15:15:17 +0800

    feat:update files

--- src/pages/daping/index.vue
+++ src/pages/daping/index.vue
@@ -2,8 +2,12 @@
   <div>大屏数据</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue';
+function test(): number {
+  let a: any = 1;
+  return 1;
+}
 </script>
 
 <style lang="scss"></style>
